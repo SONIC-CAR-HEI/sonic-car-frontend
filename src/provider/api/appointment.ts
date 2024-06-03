29126a1fd89a14bf83592aae8319e41a2be7c55b
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-03 15:40:42 +0300

    chore: implement api calls

---
+++ src/provider/api/appointment.ts
@@ -0,0 +1,41 @@
+import { BaseApi } from '@/src/provider/api/baseApi';
+import { apiClient } from '@/src/provider/client';
+
+export type AppointmentStatus =
+  | 'PENDING'
+  | 'ARCHIVED'
+  | 'REJECTED'
+  | 'ACCEPTED';
+
+export interface AppointmentData {
+  id: string;
+  status: AppointmentStatus;
+  firstName: string;
+  lastName: string;
+  message: string;
+  email: string;
+  tel: string;
+  date: Date;
+  carId: string;
+  adminId: string;
+}
+
+export type AppointmentCreatePayload = Omit<AppointmentData, 'adminId' | 'id'>;
+
+export class Appointment extends BaseApi<
+  AppointmentData['id'],
+  AppointmentData,
+  AppointmentCreatePayload
+> {
+  constructor() {
+    super('appointment');
+  }
+
+  async updateStatus(
+    id: AppointmentData['id'],
+    status: AppointmentStatus,
+  ): Promise<AppointmentData> {
+    return (await apiClient.post(this.resource_name + id + '?status=' + status))
+      .data;
+  }
+}
---
+++ src/provider/api/car.ts
@@ -0,0 +1,104 @@
+import { BaseApi } from '@/src/provider/api/baseApi';
+import { apiClient } from '@/src/provider/client';
+
+export type EngineType = 'DIESEL' | 'HYBRID' | 'ELECTRIC' | 'GASOLINE';
+
+export interface CarData {
+  id: string;
+  model: string;
+  name: string;
+  description: string;
+  price: number;
+  engineType: EngineType;
+  placeNumber: number;
+  color: string;
+  power: number;
+  available: boolean;
+  fav: boolean;
+  typeId: string;
+  brandId: string;
+}
+
+export type CarCreatePayload = Omit<CarData, 'id'>;
+
+export class Car extends BaseApi<CarData['id'], CarData, CarCreatePayload> {
+  constructor() {
+    super('car');
+  }
+}
+
+export interface CarImagePayload {
+  image: File;
+}
+
+export interface CarImageData {
+  id: string;
+  imageUrl: string;
+  carId: string;
+}
+
+export interface CarUpdatePayload {
+  imageUrl: string;
+  carId: string;
+}
+
+export class CarImage extends BaseApi<
+  CarImageData['id'],
+  CarImageData,
+  CarImagePayload,
+  CarUpdatePayload
+> {
+  constructor() {
+    super('car-image');
+  }
+
+  /**
+   * @deprecated use createImage() instead
+   * @param data
+   */
+  async create(data: CarImagePayload): Promise<CarImageData> {
+    throw new Error('cannot use this method, use createImage() instead');
+  }
+
+  async createImage(
+    carId: CarImageData['id'],
+    data: CarImagePayload,
+  ): Promise<CarImageData> {
+    const response = await apiClient.post(
+      this.resource_name + '/' + carId,
+      data,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    return response.data;
+  }
+
+  async get(id: CarImageData['id']): Promise<CarImageData> {
+    throw new Error('not implemented');
+  }
+
+  async getCarImagesUrl(carId: CarData['id']): Promise<string[]> {
+    return (await apiClient.get(this.resource_name + '/' + carId)).data;
+  }
+}
+
+export interface CarTypeData {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export type CarTypeCreateDto = Omit<CarTypeData, 'id'>;
+
+export class CarType extends BaseApi<
+  CarTypeData['id'],
+  CarTypeData,
+  CarTypeCreateDto
+> {
+  constructor() {
+    super('car-type');
+  }
+}
